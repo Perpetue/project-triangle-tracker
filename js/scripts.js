6f251eb119bd85a661f8bca07e22bb81c005041e
Perpetue <muperpety@gmail.com>
2018-12-13 18:03:29 +0200

    add the variable in file csripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ function myFunction() {
     var firstString =document.getElementById("myNumber").value;
      alert(firstString);
 }
+var triangle=["lenght1","lenght2","lenght3"];
+var lenght1=parseInt(prompt("enter lenght1"));
+var lenght2=parseInt(prompt("enter lenght2"));
+var lenght3=parseInt(prompt("enter lenght3"));
+console.log([lenght1, lenght2,lenght3]);
