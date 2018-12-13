aa8583c92e881fc92243ed6bd084bde22fd5f33b
Perpetue <muperpety@gmail.com>
2018-12-13 18:09:12 +0200

    add the statement if else in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,15 @@ console.log([lenght1, lenght2,lenght3]);
 function triangle(lenght1,lenght2,lenght3){
     return triangle(lenght1,lenght2,lenght3);
     }
+    if (lenght1==lenght2 && lenght1==lenght3 && lenght2==lenght3){
+        console.log("Equilateral triangle");
+        }
+        else if (lenght1==lenght2||lenght1==lenght3||lenght2==lenght3){
+        console.log("Isoceles triangle");
+        }
+        else if (lenght1+lenght2 > lenght3 && lenght1+ lenght3>lenght2 && lenght2+lenght3>lenght1) {
+        console.log("Scalene triangle");
+        }
+        else {
+        console.log("Not a triangle");
+        }
