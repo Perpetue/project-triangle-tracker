17ccb01c72993a3a50dd108444039a8b663f4138
Perpetue <muperpety@gmail.com>
2018-12-14 14:36:57 +0200

    add arrays in file of scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
-function myFunction() {
+function myFunction () {
     var lenght1 = parseInt(document.getElementById("lenght1").value);
     var lenght2 = parseInt(document.getElementById("lenght2").value);
     var lenght3 = parseInt(document.getElementById("lenght3").value);
-    if (lenght1===lenght2 && lenght1===lenght3 && lenght2===lenght3){
+    var triangle=[lenght1,lenght2,lenght3];
+    console.log( triangle);
+    if (lenght1+lenght2 > lenght3 && lenght1+ lenght3>lenght2 && lenght2+lenght3>lenght1) {
+
+    if (lenght1===lenght2 && lenght1===lenght3 && lenght2===lenght3) {
         alert("Equilateral triangle");
+        console.log("Equilateral triangle ");
         }
         else if (lenght1===lenght2||lenght1===lenght3||lenght2===lenght3){
         alert("Isoceles triangle"); 
         console.log("Isoceles triangle");
         }
-        else if (lenght1+lenght2 > lenght3 && lenght1+ lenght3>lenght2 && lenght2+lenght3>lenght1) {
+        else {
         alert("Scalene triangle"); 
         console .log ("Scalene triangle");
         }
-        else {
+          }
+         else  {
         alert("Not a triangle"); 
         console.log("Not a triangle");
         }
-    }
+      };
