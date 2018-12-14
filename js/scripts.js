ae9090e2002da6b8db251b851b9d9bb526ddc239
Perpetue <muperpety@gmail.com>
2018-12-14 12:40:13 +0200

    add the variable changed in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 function myFunction() {
-    var lenght1 =document.getElementById("myNumber").value1;
-    var lenght2=document.getElementById("myNumber").value2;
-    var lenght3=document.getElementById("myNumber").value2;
-     alert (lenght1,lenght2,lenght3);
-      }
-    if (lenght1==lenght2 && lenght1==lenght3 && lenght2==lenght3){
-        console.log("Equilateral triangle");
+    var lenght1 = parseInt(document.getElementById("lenght1").value);
+    var lenght2 = parseInt(document.getElementById("lenght2").value);
+    var lenght3 = parseInt(document.getElementById("lenght3").value);
+    if (lenght1===lenght2 && lenght1===lenght3 && lenght2===lenght3){
+        alert("Equilateral triangle");
         }
-        else if (lenght1==lenght2||lenght1==lenght3||lenght2==lenght3){
+        else if (lenght1===lenght2||lenght1===lenght3||lenght2===lenght3){
+        alert("Isoceles triangle"); 
         console.log("Isoceles triangle");
         }
         else if (lenght1+lenght2 > lenght3 && lenght1+ lenght3>lenght2 && lenght2+lenght3>lenght1) {
-        console.log("Scalene triangle");
+        alert("Scalene triangle"); 
+        console .log ("Scalene triangle");
         }
         else {
+        alert("Not a triangle"); 
         console.log("Not a triangle");
         }
-    
+    }
