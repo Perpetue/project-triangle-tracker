4c0b9b512458290e1e7a6b5d432c62bcae9310a0
Perpetue <muperpety@gmail.com>
2018-12-17 18:23:37 +0200

    added all changes

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,20 @@ function myFunction () {
     console.log( triangle);
     if (lenght1+lenght2 > lenght3 && lenght1+ lenght3>lenght2 && lenght2+lenght3>lenght1) {
     if (lenght1===lenght2 && lenght1===lenght3 && lenght2===lenght3) {
+      console.log("Equilateral triangle ");
         alert("Equilateral triangle");
-        console.log("Equilateral triangle ");
         }
         else if (lenght1===lenght2||lenght1===lenght3||lenght2===lenght3){
-        alert("Isoceles triangle"); 
         console.log("Isoceles triangle");
+        alert("Isoceles triangle"); 
         }
-        else {
-        alert("Scalene triangle"); 
+        else { 
         console .log ("Scalene triangle");
+        alert("Scalene triangle");
         }
           }
          else  {
-        alert("Not a triangle"); 
         console.log("Not a triangle");
+        alert("Not a triangle"); 
         }
       };
